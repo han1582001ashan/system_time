a5f33c9b67dd8e11a804997d7845ef546af245b4
han1582001ashan <83277320+han1582001ashan@users.noreply.github.com>
2024-09-26 16:48:47 +0700

    nopbai

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace SystemTime
+{
+    class Program{
+        static void Main(string[] args){
+           DateTime localDate = DateTime.Now;
+           Console.WriteLine("Now is: "+ localDate);
+        }
+    }
+}
